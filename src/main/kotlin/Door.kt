04a99809801ec4b315763b464dfd908c979d72aa
user <user@example.com>
2018-08-02 20:11:40 +0300

    Add pause to Door.open() and Door.close() methods

--- src/main/kotlin/Door.kt
+++ src/main/kotlin/Door.kt
@@ -1,16 +1,18 @@
 import kotlin.properties.Delegates
 
-open class Door(val speed:Int = 500){
+open class Door(val speed:Long = 500){
     var state by Delegates.observable(DoorState.CLOSED){
         _, _, newValue ->
         println("The door is ${newValue.toString().toLowerCase()}")
     }
     
     fun open(){
+        Thread.sleep(speed)
         if(state!=DoorState.OPENED)
         state = DoorState.OPENED
     }
     fun close() {
+        Thread.sleep(speed)
         if(state!=DoorState.CLOSED)
         state = DoorState.CLOSED
     }
